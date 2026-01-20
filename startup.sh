16773fe44d4201555a4a3e0404f714dd2ee10347
stenny94 <101674800+stenny94@users.noreply.github.com>
2026-01-20 06:46:31 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-wget -O- get.docker.com | bash
+sleep 3
 
-sudo apt-get install -y uidmap
-
-dockerd-rootless-setuptool.sh install
-
-docker run -d -p 8080:80 --name kiosk-menu --restart=always stenny94/kiosk-menu:latest
+firefox --kiosk --new-window http://localhost:8080
