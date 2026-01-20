1cc668e124890dbcd1d6adf368ad302fa2e36d6b
stenny94 <101674800+stenny94@users.noreply.github.com>
2026-01-20 07:06:02 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,8 @@ sudo apt-get install -y uidmap xautomation unclutter
 
 dockerd-rootless-setuptool.sh install
 
+sudo wget -O /startup.sh https://raw.githubusercontent.com/stenny94/kiosk-menu/refs/heads/main/startup.sh
+
+sudo chmod a+x /startup.sh
+
 docker run -d -p 8080:80 --name kiosk-menu --restart=always stenny94/kiosk-menu:latest
