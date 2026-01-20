08461fd0faa43b18f1cd973dd98c8a28e602b659
stenny94 <101674800+stenny94@users.noreply.github.com>
2026-01-19 19:56:50 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
 
-# Function for the spinning animation
-spinner() {
-    local pid=$1
-    local delay=0.1
-    local spinstr='|/-\'
-    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-        local temp=${spinstr#?}
-        printf " [%c]  " "$spinstr"
-        local spinstr=$temp${spinstr%"$temp"}
-        sleep $delay
-        printf "\b\b\b\b\b\b"
-    done
-    printf "    \b\b\b\b"
-}
-
 echo "Step 1: Installing Docker..."
-(wget -O- get.docker.com | bash) > /dev/null 2>&1 &
-spinner $!
+wget -O- get.docker.com | bash
 
 echo "Step 2: Installing uidmap..."
-(sudo apt-get install -y uidmap) > /dev/null 2>&1 &
-spinner $!
+sudo apt-get install -y uidmap
 
 echo "Step 3: Running Rootless setup tool..."
-(dockerd-rootless-setuptool.sh install) > /dev/null 2>&1 &
-spinner $!
+dockerd-rootless-setuptool.sh install
 
 echo "Step 4: Starting the Kiosk Menu container..."
-(docker run -d -p 8080:80 --name kiosk-menu --restart=always stenny94/kiosk-menu:latest) > /dev/null 2>&1 &
-spinner $!
+docker run -d -p 8080:80 --name kiosk-menu --restart=always stenny94/kiosk-menu:latest
 
 echo "Setup complete!"
