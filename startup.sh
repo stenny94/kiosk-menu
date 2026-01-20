eff246bf59fec28ac10ff0649017d7f8fb26d631
stenny94 <101674800+stenny94@users.noreply.github.com>
2026-01-20 06:44:10 -0600

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget -O- get.docker.com | bash
+
+sudo apt-get install -y uidmap
+
+dockerd-rootless-setuptool.sh install
+
+docker run -d -p 8080:80 --name kiosk-menu --restart=always stenny94/kiosk-menu:latest
