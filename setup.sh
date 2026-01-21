90d89cd27465f0abb22cdec1273ffe7f76109004
stenny94 <101674800+stenny94@users.noreply.github.com>
2026-01-20 22:03:07 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,21 @@ sudo chmod a+x /startup.sh
 
 docker rm -f kiosk-menu
 
+echo ""
+echo ""
 echo "--------------------------------------------------"
 read -p "Please enter your store location (e.g., clay, beloit, st_marys): " STORELOCATION < /dev/tty
 echo "Setting location to: $STORELOCATION"
 echo "--------------------------------------------------"
+echo ""
+echo ""
+echo "--------------------------------------------------"
+read -p "Please enter your the type of file needed (e.g., image, video) " FILETYPE < /dev/tty
+echo "Setting filetype to: $FILETYPE"
+echo "--------------------------------------------------"
+echo ""
+echo ""
 
-docker run -d -p 8080:80 --name kiosk-menu --restart=always -e LOCATION="$STORELOCATION" stenny94/kiosk-menu:latest
+
+
+docker run -d -p 8080:80 --name kiosk-menu --restart=always -e LOCATION="$STORELOCATION" -e FILETYPE="$FILETYPE" stenny94/kiosk-menu:latest
