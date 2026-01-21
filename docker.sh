b9cd8cbcd425dafb08248f98e597f21c3425ece2
stenny94 <101674800+stenny94@users.noreply.github.com>
2026-01-21 11:53:15 -0600

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install -y uidmap xautomation unclutter curl wget
+
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
 dockerd-rootless-setuptool.sh install
-
-sudo apt-get update
-sudo apt-get install -y uidmap xautomation unclutter
