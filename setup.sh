e037e8622acd6606808c67d7e4a0b338f9e22d3d
stenny94 <101674800+stenny94@users.noreply.github.com>
2026-01-20 21:47:27 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo wget -O /startup.sh https://raw.githubusercontent.com/stenny94/kiosk-menu/refs/heads/main/startup.sh
+sudo chmod a+x /startup.sh
+
+docker rm -f kiosk-menu
+
+echo "--------------------------------------------------"
+read -p "Please enter your store location (e.g., clay, beloit, st_marys): " STORELOCATION < /dev/tty
+echo "Setting location to: $STORELOCATION"
+echo "--------------------------------------------------"
+
+docker run -d -p 8080:80 --name kiosk-menu --restart=always -e LOCATION="$STORELOCATION" stenny94/kiosk-menu:latest
