13437cf00c6170f061594a670b8b664fcf9943cb
stenny94 <101674800+stenny94@users.noreply.github.com>
2026-01-20 21:22:08 -0600

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,7 +8,8 @@ mkdir html
 git clone https://github.com/stenny94/kiosk-menu.git html
 
 cd html
-sed -i 's/*LOCATION/${LOCATION:-clay}/g' index.html
+sed -i "s/*LOCATION/${LOCATION:-clay}/g" index.html
 
 nginx -g "daemon off;"
+
 
