906a736fe37b99d1e18280f8672f08fb5bc06825
stenny94 <101674800+stenny94@users.noreply.github.com>
2026-01-20 20:54:35 -0600

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Navigate to the parent directory
 cd /usr/share/nginx
 
-# Force delete the html directory and recreate it empty
 rm -rf html
 mkdir html
 
-# Clone the repo into the now-empty html folder
 git clone https://github.com/stenny94/kiosk-menu.git html
 
-# Start Nginx
+cd html
+sed -i 's/LOCATION/${APP_MODE:-clay}/g' index.html
+
 nginx -g "daemon off;"
