18d68d00fc30d7c7aa0b30828236ab29c1032448
stenny94 <101674800+stenny94@users.noreply.github.com>
2026-01-21 15:05:45 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,29 +11,7 @@ echo "Setting location to: $STORELOCATION"
 echo "--------------------------------------------------"
 echo ""
 echo ""
-echo "--------------------------------------------------"
-read -p "Please enter the type of file needed (image or video): " USER_INPUT < /dev/tty
-
-# Convert input to the desired extension
-case "$USER_INPUT" in
-  video)
-    FILETYPE=".avi"
-    ;;
-  image)
-    FILETYPE=".png"
-    ;;
-  *)
-    # Fallback if they type something else or leave it blank
-    echo "Invalid input detected. Defaulting to .png"
-    FILETYPE=".png"
-    ;;
-esac
-
-echo "Setting filetype to: $FILETYPE"
-echo "--------------------------------------------------"
-echo ""
-echo ""
 
 
 
-docker run -d -p 8080:80 --name kiosk-menu --restart=always -e LOCATION="$STORELOCATION" -e FILETYPE="$FILETYPE" stenny94/kiosk-menu:latest
+docker run -d -p 8080:80 --name kiosk-menu --restart=always -e LOCATION="$STORELOCATION" stenny94/kiosk-menu:latest
